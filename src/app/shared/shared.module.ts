d9bb72a305d49273a1490001c5b10e1a9a486db7
diegoperezeng <diegoperez.eng@gmail.com>
2023-01-07 21:21:52 -0300

    Adding job-search component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 // Components
 import { JobHeaderComponent } from './job-header/job-header.component';
+import { JobSearchComponent } from './job-search/job-search.component';
 
 
 
 @NgModule({
   declarations: [
-    JobHeaderComponent
+    JobHeaderComponent,
+    JobSearchComponent
   ],
   exports: [
     JobHeaderComponent
