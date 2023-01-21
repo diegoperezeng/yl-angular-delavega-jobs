88fddfd3447404a7adf498e37e6de18544eeea8c
diegoperezeng <diegoperez.eng@gmail.com>
2023-01-21 00:36:30 -0300

    Add new component: job-list

---
+++ src/app/shared/job-list/job-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yl-job-list',
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.scss']
+})
+export class JobListComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 // Components
 import { JobHeaderComponent } from './job-header/job-header.component';
 import { JobContentComponent } from './job-content/job-content.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 
 
 @NgModule({
   declarations: [
     JobHeaderComponent,
-    JobContentComponent
+    JobContentComponent,
+    JobListComponent
   ],
   exports: [
     JobHeaderComponent,
